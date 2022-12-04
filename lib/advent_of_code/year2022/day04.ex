4ba82f9f8d7c45ed966deaf571571a69e52be818
Cullan Springstead <cullan@springstead.me>
2022-12-04 09:30:10 -0600

    solve 2022 day 04

---
+++ lib/advent_of_code/year2022/day04.ex
@@ -0,0 +1,47 @@
+defmodule AdventOfCode.Year2022.Day04 do
+  @moduledoc """
+  --- Day 4: Camp Cleanup ---
+  https://adventofcode.com/2022/day/4
+  """
+
+  def section_assignment_pairs(input) do
+    input
+    |> String.split()
+    |> Enum.map(fn pair ->
+      pair
+      |> String.split(",")
+      |> Enum.map(&section_assignment_range/1)
+    end)
+  end
+
+  def section_assignment_range(s), do: s |> String.split("-") |> Enum.map(&String.to_integer/1)
+
+  def fully_overlaps?([[elf1_begin, elf1_end], [elf2_begin, elf2_end]]) do
+    cond do
+      elf1_begin >= elf2_begin and elf1_end <= elf2_end -> true
+      elf2_begin >= elf1_begin and elf2_end <= elf1_end -> true
+      true -> false
+    end
+  end
+
+  def overlaps?([[elf1_begin, elf1_end], [elf2_begin, elf2_end]]) do
+    not Range.disjoint?(
+      Range.new(elf1_begin, elf1_end),
+      Range.new(elf2_begin, elf2_end)
+    )
+  end
+
+  def part1(input) do
+    input
+    |> section_assignment_pairs()
+    |> Enum.map(&fully_overlaps?/1)
+    |> Enum.count(&(&1))
+  end
+
+  def part2(input) do
+    input
+    |> section_assignment_pairs()
+    |> Enum.map(&overlaps?/1)
+    |> Enum.count(&(&1))
+  end
+end
---
+++ test/advent_of_code/year2022/day04_test.exs
@@ -0,0 +1,22 @@
+defmodule AdventOfCode.Year2022.Day04Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Year2022.Day04
+
+  @input """
+  2-4,6-8
+  2-3,4-5
+  5-7,7-9
+  2-8,3-7
+  6-6,4-6
+  2-6,4-8
+  """
+
+  test "part1" do
+    assert part1(@input) == 2
+  end
+
+  test "part2" do
+    assert part2(@input) == 4
+  end
+end
