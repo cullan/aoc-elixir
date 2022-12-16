0b52980d894e7c67170e50026fda295d6348f8f2
Cullan Springstead <cullan@springstead.me>
2022-12-15 23:03:06 -0600

    import day module and alias Grid and Math modules

--- .iex.exs
+++ .iex.exs
@@ -2,11 +2,15 @@ defmodule IExHelpers do
   defmacro workon(day, year \\ DateTime.utc_now().year) do
     quote do
       alias unquote(:"Elixir.AdventOfCode.Year#{year}.Day#{AdventOfCode.zero_pad(day)}")
+      import unquote(:"Elixir.AdventOfCode.Year#{year}.Day#{AdventOfCode.zero_pad(day)}")
     end
   end
 end
 
 import IExHelpers
+
+alias AdventOfCode.Grid
+alias AdventOfCode.Math
 
 # default to displaying charlists as lists
 # IO.inspect(item, charlists: :infer) can be used to manually view charlists
